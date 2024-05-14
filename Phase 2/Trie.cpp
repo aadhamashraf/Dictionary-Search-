078deb02d0e8c1895fd1712e02f72394a3bd1ad2
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2024-05-14 05:09:04 +0300

    Update Trie.cpp

--- Phase 2/Trie.cpp
+++ Phase 2/Trie.cpp
@@ -1,40 +1,58 @@
 #include "Trie.h"
+#include "Vector.h"
 
-Trie::Trie() : root(new TrieNode()) {}
+Trie::Trie() { root = new TrieNode();}
 
-void Trie::insert(std::string word) {
-    TrieNode* node = root;
+void Trie::insert(const std::string &word) {
+    TrieNode *current = root;
     for (char ch : word) {
         int index = ch - 'a';
-        if (!node->children[index]) {
-            node->children[index] = new TrieNode();
+        if (!current->children[index]) {
+            current->children[index] = new TrieNode();
         }
-        node = node->children[index];
+        current = current->children[index];
     }
-    node->isEndOfWord = true;
+    current->isEndOfWord = true;
 }
 
-void Trie::displayUtil(TrieNode* node, std::string str) {
-    if (node->isEndOfWord) {
-        std::cout << str << std::endl;
+bool Trie::search(const std::string &word) {
+    TrieNode *current = root;
+    for (char ch : word) {
+        int index = ch - 'a';
+        if (!current->children[index]) { return false;}
+        current = current->children[index];
     }
-    for (char ch = 'a'; ch <= 'z'; ch++) {
-        TrieNode* nextNode = node->children[ch - 'a'];
-        if (nextNode) {
-            displayUtil(nextNode, str + ch);
+    return current && current->isEndOfWord;
+}
+
+bool Trie::isEmpty(TrieNode *node) {
+    for (int i = 0; i < ALPHABET_SIZE; ++i) { if (node->children[i]) { return false;} }
+    return true;
+}
+
+TrieNode* Trie::remove(TrieNode *node, const std::string &word, int depth) {
+    
+    if (!node) { return nullptr;}
+
+    if (depth == word.size()) { if (node->isEndOfWord) { node->isEndOfWord = false;  }
+
+        if (isEmpty(node)) {
+            delete node;
+            node = nullptr;
         }
+
+        return node;
     }
+
+    int index = word[depth] - 'a';
+    node->children[index] = remove(node->children[index], word, depth + 1);
+
+    if (isEmpty(node) && !node->isEndOfWord) {
+        delete node;
+        node = nullptr;
+    }
+
+    return node;
 }
 
-void Trie::display() {
-    displayUtil(root, "");
-}
-
-Vector Trie::fuzzySearch(std::string pattern) {
-}
-
-Vector Trie::misspelledWordSearch(std::string word) {
-}
-
-Vector Trie::partialWordSearch(std::string suffix) {
-}
+void Trie::deleteWord(const std::string &word) {remove(root, word);}
