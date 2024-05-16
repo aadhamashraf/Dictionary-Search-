562d9ed0195b9e5438e7e0a79821cb677f58cb2a
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2024-05-16 21:34:09 +0300

    Update main.cpp

--- Phase 2/main.cpp
+++ Phase 2/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
-#include "Trie.h"
+#include "Dictionary_Search.h"
 
-int main() {}
+int main() {
+
+    std::string filePath = "C:\\Users\\Ashraf\\CLionProjects\\Phase 2\\corpus.txt";
+
+    Dictionary_Search searchEngine;
+    searchEngine.indexCorpus(filePath);
+    searchEngine.performSearch();
+
+    return 0;
+}
