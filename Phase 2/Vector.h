a7527f7872cd6cf9301ad599d800512ac3d8a22d
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2024-05-18 13:29:41 +0300

    Update Vector.h

--- Phase 2/Vector.h
+++ Phase 2/Vector.h
@@ -1,18 +1,26 @@
-#pragma once
-#include <iostream>
+// Vector.h
 
+#ifndef VECTOR_H
+#define VECTOR_H
+#include <iostream>
+#include <string>
 
 class Vector {
 private:
-    std::string * array;
-    int size;
-    int capacity;
+    std::string* array;
+    size_t size , capacity;
 
 public:
     Vector();
+
+    void display()const;
     void push_back(const std::string& item);
-    void display() const;
+    std::string& operator[](size_t index);
+    const std::string& operator[](size_t index) const;
+    size_t getSize() const;
+
     ~Vector();
 
 };
 
+#endif // VECTOR_H
