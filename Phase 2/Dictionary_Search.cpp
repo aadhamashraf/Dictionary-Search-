3c9a6780f7bc9f6e90a0e0059fb6515f6e4f53eb
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2024-05-18 13:28:31 +0300

    Update Dictionary_Search.cpp

--- Phase 2/Dictionary_Search.cpp
+++ Phase 2/Dictionary_Search.cpp
@@ -11,44 +11,37 @@ void Dictionary_Search::indexCorpus(const std::string& filePath) {
         return;
     }
 
-    while ( std::getline(file, word) ) {
-        trie.insert(word);
-    }
+    while (std::getline(file, word)) { trie.insert(word); }
 
     file.close();
 
     std::cout << "Corpus indexed successfully." << std::endl;
 }
 
-Vector Dictionary_Search::fuzzySearch(const std::string& searchTerm) {
-    return trie.fuzzySearch(searchTerm);
-}
+Vector Dictionary_Search::fuzzySearch(const std::string& searchTerm) { return trie.fuzzySearch(searchTerm); }
 
-Vector Dictionary_Search::misspelledWordSearch(const std::string& searchTerm) {
-    return trie.misspelledWordSearch(searchTerm);
-}
+Vector Dictionary_Search::misspelledWordSearch(const std::string& searchTerm) { return trie.misspelledWordSearch(searchTerm); }
 
-Vector Dictionary_Search::partialWordSearch(const std::string& searchTerm) {
-    return trie.partialWordSearch(searchTerm);
-}
+Vector Dictionary_Search::partialWordSearch(const std::string& searchTerm) { return trie.partialWordSearch(searchTerm); }
 
-Vector Dictionary_Search::wildcardSearch(const std::string& searchTerm) {
-    return trie.wildcardSearch(searchTerm);
-}
+// Vector For The 4 Searches
 
 void Dictionary_Search::performSearch() {
     while (true) {
-        int choice {-1};
-        std::string searchTerm {""};
-        Vector searchResult;
+        int choice{-1};
+        std::string searchTerm{""};
 
         std::cout << "Menu : " << std::endl
+                  << std::endl
                   << "1) Show The Trie Data Structure " << std::endl
-                  << "2) Fuzzy Search " << std::endl
-                  << "3) Misspelled Word Search " << std::endl
-                  << "4) Partial Word Search(Suffix) " << std::endl
-                  << "5) Wildcard Search " << std::endl
-                  << "6) Exit The Program " << std::endl
+                  << "2) Single Word Search "<<std::endl
+                  << "3) Multiple Word Search "<<std::endl
+                  << "4) Partial Word Search(Prefix) "<<std::endl
+                  << "5) Fuzzy Search " << std::endl
+                  << "6) Misspelled Word Search " << std::endl
+                  << "7) Partial Word Search(Suffix) " << std::endl
+                  << "8) Wildcard Search "<<std::endl
+                  << "9) Exit The Program " << std::endl
                   << "Enter Your Choice Here: ";
         std::cin >> choice;
 
@@ -58,7 +51,7 @@ void Dictionary_Search::performSearch() {
 
         switch (choice) {
             case 1: {
-                int choice_1  {-1};
+                int choice_1{-1};
                 std::cout << "Trie Capabilities Menu: " << std::endl
                           << "1.1) Display The Trie Data Structure " << std::endl
                           << "1.2) Add Single Element " << std::endl
@@ -68,104 +61,95 @@ void Dictionary_Search::performSearch() {
                 std::cin >> choice_1;
 
                 switch (choice_1) {
-                    case 1:
+                    case 1:{
                         start = std::chrono::high_resolution_clock::now();
+                        std::cout<<"Here is the Trie Content : "<<std::endl;
                         trie.displayContent();
                         end = std::chrono::high_resolution_clock::now();
                         duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
                         std::cout << "Time taken by this function: " << duration.count() << " seconds" << std::endl;
                         std::cout << "Number of words inside the folder: " << trie.get_trie_size() << std::endl;
                         break;
+                    }
                     case 2: {
-                        std::string tempword {""};
+                        std::string tempword{""};
                         std::cout << "Enter The Word You Want To Insert: ";
                         std::cin >> tempword;
-                        start = std::chrono::high_resolution_clock::now();
-                        trie.insert(tempword);
-                        end = std::chrono::high_resolution_clock::now();
-                        duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-                        std::cout << "Time taken by this function: " << duration.count() << " seconds" << std::endl;
+                        std::cout<<std::endl;
+                        bool success = trie.insert(tempword);
+                        (success) ? std::cout << "Word inserted successfully" << std::endl : std::cout << "Word already exists" << std::endl;
                         break;
                     }
                     case 3: {
                         std::string tempword{""};
                         std::cout << "Enter the Word You Want to Search for: ";
                         std::cin >> tempword;
-                        start = std::chrono::high_resolution_clock::now();
+                        std::cout<<std::endl;
                         bool result = trie.search(tempword);
-                        end = std::chrono::high_resolution_clock::now();
-                        duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-                        std::cout << (result ? "Existed" : "Not Existed") << std::endl;
-                        std::cout << "Time taken by this function: " << duration.count() << " seconds" << std::endl;
+                        (result) ?std::cout<<"Word Founded Elhamdullah "<<std::endl : std::cout<<"Word Isn't Founded In The Corpus"<<std::endl;
                         break;
                     }
                     case 4: {
                         std::string tempword{""};
                         std::cout << "Enter the Word You Want to Delete: ";
                         std::cin >> tempword;
-                        start = std::chrono::high_resolution_clock::now();
-                        trie.deleteWord(tempword);
-                        end = std::chrono::high_resolution_clock::now();
-                        duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-                        std::cout << "Time taken by this function: " << duration.count() << " seconds" << std::endl;
+                        std::cout<<std::endl;
+                        bool success = trie.deleteWord(tempword);
+                        (success) ? std::cout << "Word deleted successfully" << std::endl : std::cout << "Word isn't founded aslun in the corpus " << std::endl;
+
                         break;
                     }
                     default:
                         std::cout << "Invalid choice. Try again." << std::endl;
+                        std::cout<<std::endl;
                         break;
                 }
                 break;
             }
-            case 2:
+
+            //Case 2 => 4
+
+            case 5: {
                 std::cout << "Enter Your Input For The Fuzzy Search: ";
                 std::cin >> searchTerm;
-                start = std::chrono::high_resolution_clock::now();
-                searchResult = fuzzySearch(searchTerm);
-                end = std::chrono::high_resolution_clock::now();
-                duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+                std::cout<<std::endl;
+                Vector fuzzysearchResult = fuzzySearch(searchTerm);
+                fuzzysearchResult.display();
+                std::cout<<std::endl;
                 break;
+            }
 
-            case 3:
+            case 6: {
                 std::cout << "Enter Your Input For The Misspelled Word Search: ";
                 std::cin >> searchTerm;
-                start = std::chrono::high_resolution_clock::now();
-                searchResult = misspelledWordSearch(searchTerm);
-                end = std::chrono::high_resolution_clock::now();
-                duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+                std::cout<<std::endl;
+                Vector misspelledsearchResult=misspelledWordSearch(searchTerm);
+                misspelledsearchResult.display();
+                std::cout<<std::endl;
                 break;
+            }
 
-            case 4:
+            case 7: {
                 std::cout << "Enter Your Input For The Partial Word Search(Suffix): ";
                 std::cin >> searchTerm;
-                start = std::chrono::high_resolution_clock::now();
-                searchResult = partialWordSearch(searchTerm);
-                end = std::chrono::high_resolution_clock::now();
-                duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+                std::cout<<std::endl;
+                Vector partialwordsearchResult = partialWordSearch(searchTerm);
+                partialwordsearchResult.display();
+                std::cout<<std::endl;
                 break;
+            }
 
-            case 5:
-                std::cout << "Enter Your Input For The Wildcard Search: ";
-                std::cin >> searchTerm;
-                start = std::chrono::high_resolution_clock::now();
-                searchResult = wildcardSearch(searchTerm);
-                end = std::chrono::high_resolution_clock::now();
-                duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-                break;
+            //Case 8
 
-            case 6:
+            case 9:{
                 std::cout << "Bye Bye! Program Will Terminate Now." << std::endl;
                 return;
-
-            default:
+            }
+            default:{
                 std::cout << "You have entered the wrong choice. Try again." << std::endl;
                 break;
+            }
         }
 
-        if (choice != 1 && choice != 6) {
-            std::cout << "Time taken by this function: " << duration.count() << " seconds" << std::endl;
-            std::cout << "Your Result: " << std::endl;
-            searchResult.display();
-            std::cout << std::endl;
-        }
     }
 }
