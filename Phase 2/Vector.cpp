34530764f9f857c08ce8d8d400ea0c8f9677fc71
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2024-05-18 13:29:52 +0300

    Update Vector.cpp

--- Phase 2/Vector.cpp
+++ Phase 2/Vector.cpp
@@ -1,12 +1,22 @@
+// Vector.cpp
+
 #include "Vector.h"
 
 Vector::Vector() : array(nullptr), size(0), capacity(0) {}
+
+void Vector::display() const {
+    std::cout << "[ ";
+    for (int i = 0; i < size; ++i) { std::cout << array[i] << "  "; }
+    std::cout << " ]" << std::endl;
+}
 
 void Vector::push_back(const std::string& item) {
     if (size >= capacity) {
         capacity = (capacity == 0) ? 1 : capacity * 2;
         std::string* newArray = new std::string[capacity];
-        for (int i = 0; i < size; ++i) { newArray[i] = array[i];}
+        for (size_t i = 0; i < size; ++i) {
+            newArray[i] = array[i];
+        }
         delete[] array;
         array = newArray;
     }
@@ -14,11 +24,11 @@ void Vector::push_back(const std::string& item) {
     array[size++] = item;
 }
 
-void Vector::display() const {
-    std::cout << "[ ";
-    for (int i = 0; i < size; ++i) { std::cout << array[i] << "  "; }
-    std::cout << " ]" << std::endl;
-}
+std::string& Vector::operator[](size_t index) { return array[index]; }
+
+const std::string& Vector::operator[](size_t index) const { return array[index]; }
+
+size_t Vector::getSize() const { return size; }
 
 Vector::~Vector() {
     delete[] array;
